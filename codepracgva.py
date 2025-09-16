de9ddfdadbdb84dbbadc00e45130256c6fb3cf3e
Glory Victor Asuquo <gloryva0@gmail.com>
2025-09-16 17:03:06 +0000

    gloryspractice

---
+++ codepracgva.py
@@ -0,0 +1,13 @@
+num1 = 1
+num2 = 3
+
+import os
+
+print(os.getenv("password"))
+print(os.getenv("apitoken"))
+num4 = 20
+num5 = 13
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2 + num4 - num5)
